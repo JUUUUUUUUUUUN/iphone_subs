8e44f1bd97165d9a5b9f0e14a46c5523d2ba6a4b
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:17:27 +0900

    Create PaymentCompleted.java

---
+++ MyPage/src/main/java/iphonesubscription/PaymentCompleted.java
@@ -0,0 +1,54 @@
+package iphonesubscription;
+
+public class PaymentCompleted extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String customer;
+    private String deviceName;
+    private Integer quantity;
+    private String paymentStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+}
