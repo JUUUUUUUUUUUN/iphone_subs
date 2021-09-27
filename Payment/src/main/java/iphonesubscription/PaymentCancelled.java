d7c97126efd5b126af29ee507b544eac7e521ef4
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:16:05 +0900

    Create PaymentCancelled.java

---
+++ Payment/src/main/java/iphonesubscription/PaymentCancelled.java
@@ -0,0 +1,34 @@
+package iphonesubscription;
+
+public class PaymentCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String paymentStatus;
+
+    public PaymentCancelled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+}
