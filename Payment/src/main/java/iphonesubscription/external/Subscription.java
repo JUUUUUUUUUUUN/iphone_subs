0073f47218f1e3588a747bb914666fc3f2fd841a
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:15:56 +0900

    Create Subscription.java

---
+++ Payment/src/main/java/iphonesubscription/external/Subscription.java
@@ -0,0 +1,56 @@
+package iphonesubscription.external;
+
+public class Subscription {
+
+    private Long id;
+    private Long orderId;
+    private String customer;
+    private String deviceName;
+    private Integer quantity;
+    private String subscriptionStatus;
+    private String paymentStatus;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getCustomer() {
+        return customer;
+    }
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+    public String getDeviceName() {
+        return deviceName;
+    }
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public String getSubscriptionStatus() {
+        return subscriptionStatus;
+    }
+    public void setSubscriptionStatus(String subscriptionStatus) {
+        this.subscriptionStatus = subscriptionStatus;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+}
