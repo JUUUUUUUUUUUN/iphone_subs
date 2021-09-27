871ac92abe2b4ce2276fc0031d7d0353ef085c59
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:16:56 +0900

    Create MyPageRepository.java

---
+++ MyPage/src/main/java/iphonesubscription/MyPageRepository.java
@@ -0,0 +1,16 @@
+package iphonesubscription;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MyPageRepository extends CrudRepository<MyPage, Long> {
+
+    List<MyPage> findByOrderId(Long orderId);
+    /*
+    List<MyPage> findByOrderId(Long orderId);
+    List<MyPage> findByOrderId(Long orderId);
+    List<MyPage> findByOrderId(Long orderId);
+    */
+}
