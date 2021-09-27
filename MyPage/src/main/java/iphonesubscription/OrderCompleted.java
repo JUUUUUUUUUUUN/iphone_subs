119730915c1fe02b088b4ea4be207be6a8c66f44
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:16:52 +0900

    Create OrderCompleted.java

---
+++ MyPage/src/main/java/iphonesubscription/OrderCompleted.java
@@ -0,0 +1,38 @@
+package iphonesubscription;
+
+public class OrderCompleted extends AbstractEvent {
+
+    private Long id;
+    private String customer;
+    private String deviceName;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
