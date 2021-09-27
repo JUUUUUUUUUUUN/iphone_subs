415df682f3a42771a37490095ca8e76488d36a67
JUUUUUUUUUUUN <88815763+JUUUUUUUUUUUN@users.noreply.github.com>
2021-09-27 11:11:58 +0900

    Create PaymentService.java

---
+++ Order/src/main/java/iphonesubscription/external/PaymentService.java
@@ -0,0 +1,20 @@
+package iphonesubscription.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@FeignClient(name="Payment", url="${api.url.payment}")
+public interface PaymentService {
+    /*
+    @RequestMapping(method= RequestMethod.GET, path="/payments")
+    public void pay(@RequestBody Payment payment);
+    */
+    
+    @RequestMapping(method = RequestMethod.POST, path = "/payments", consumes = "application/json")
+    public void pay(@RequestBody Payment payment);
+
+}
+
